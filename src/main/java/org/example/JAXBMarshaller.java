d71ae444e0fef2f6c817f9782a94639245a53ea0
Jonathan Thompson <90989182+jonnyfromralphs@users.noreply.github.com>
2023-06-22 11:50:23 -0700

    Update src/main/java/org/example/JAXBMarshaller.java
    Co-authored-by: jishaGithub <131222172+jishaGithub@users.noreply.github.com>

--- src/main/java/org/example/JAXBMarshaller.java
+++ src/main/java/org/example/JAXBMarshaller.java
@@ -6,7 +6,6 @@ import org.example.model.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
